b09728f75936c417a53aa555b47cefd48a43f062
atsuage333 <127850785+atsuage333@users.noreply.github.com>
2024-02-04 19:57:25 +0900

    add tycoon

--- main.lua
+++ main.lua
@@ -308,6 +308,199 @@ end
                     end    
                 })
 
+                BadgeTab:AddButton({
+                    Name = "Get Chain (Needs UnlockSlapRoyal)",
+                    Callback = function()
+                if game.Players.LocalPlayer.leaderstats.Slaps.Value >= 1000 then
+                local teleportFunc = queueonteleport or queue_on_teleport
+                if teleportFunc then
+                    teleportFunc([[
+                        if not game:IsLoaded() then
+                            game.Loaded:Wait()
+                        end
+                        repeat wait() until game.Players.LocalPlayer
+                 repeat wait() until game.Workspace:FindFirstChild("Map"):FindFirstChild("CodeBrick")
+                if game.Workspace.Map.CodeBrick.SurfaceGui:FindFirstChild("IMGTemplate") then
+                game.Workspace.Map.CodeBrick.SurfaceGui.IMGTemplate.Name = "1st"
+                game.Workspace.Map.CodeBrick.SurfaceGui.IMGTemplate.Name = "2nd"
+                game.Workspace.Map.CodeBrick.SurfaceGui.IMGTemplate.Name = "3rd"
+                game.Workspace.Map.CodeBrick.SurfaceGui.IMGTemplate.Name = "4th"
+                end
+                for i,v in pairs(game.Workspace.Map.CodeBrick.SurfaceGui:GetChildren()) do
+                                    if v.Name == "1st" then
+                                        if v.Image == "http://www.roblox.com/asset/?id=9648769161" then
+                                    first = "4"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648765536" then
+                                    first = "2"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648762863" then
+                                    first = "3"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648759883" then
+                                    first = "9"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648755440" then
+                                    first = "8"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648752438" then
+                                    first = "2"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648749145" then
+                                    first = "8"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648745618" then
+                                    first = "3"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648742013" then
+                                    first = "7"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648738553" then
+                                    first = "8"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648734698" then
+                                    first = "2"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648730082" then
+                                    first = "6"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648723237" then
+                                    first = "3"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648718450" then
+                                    first = "6"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648715920" then
+                                    first = "6"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648712563" then
+                                    first = "2"
+                                end
+                                    end
+                                end
+                for i,v in pairs(game.Workspace.Map.CodeBrick.SurfaceGui:GetChildren()) do
+                                    if v.Name == "2nd" then
+                                        if v.Image == "http://www.roblox.com/asset/?id=9648769161" then
+                                    second = "4"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648765536" then
+                                    second = "2"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648762863" then
+                                    second = "3"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648759883" then
+                                    second = "9"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648755440" then
+                                    second = "8"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648752438" then
+                                    second = "2"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648749145" then
+                                    second = "8"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648745618" then
+                                    second = "3"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648742013" then
+                                    second = "7"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648738553" then
+                                    second = "8"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648734698" then
+                                    second = "2"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648730082" then
+                                    second = "6"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648723237" then
+                                    second = "3"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648718450" then
+                                    second = "6"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648715920" then
+                                    second = "6"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648712563" then
+                                    second = "2"
+                                end
+                                    end
+                                end
+                for i,v in pairs(game.Workspace.Map.CodeBrick.SurfaceGui:GetChildren()) do
+                                    if v.Name == "3rd" then
+                                        if v.Image == "http://www.roblox.com/asset/?id=9648769161" then
+                                    third = "4"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648765536" then
+                                    third = "2"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648762863" then
+                                    third = "3"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648759883" then
+                                    third = "9"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648755440" then
+                                    third = "8"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648752438" then
+                                    third = "2"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648749145" then
+                                    third = "8"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648745618" then
+                                    third = "3"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648742013" then
+                                    third = "7"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648738553" then
+                                    third = "8"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648734698" then
+                                    third = "2"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648730082" then
+                                    third = "6"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648723237" then
+                                    third = "3"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648718450" then
+                                    third = "6"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648715920" then
+                                    third = "6"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648712563" then
+                                    third = "2"
+                                end
+                                    end
+                                end
+                for i,v in pairs(game.Workspace.Map.CodeBrick.SurfaceGui:GetChildren()) do
+                                    if v.Name == "4th" then
+                                        if v.Image == "http://www.roblox.com/asset/?id=9648769161" then
+                                    fourth = "4"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648765536" then
+                                    fourth = "2"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648762863" then
+                                    fourth = "3"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648759883" then
+                                    fourth = "9"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648755440" then
+                                    fourth = "8"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648752438" then
+                                    fourth = "2"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648749145" then
+                                    fourth = "8"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648745618" then
+                                    fourth = "3"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648742013" then
+                                    fourth = "7"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648738553" then
+                                    fourth = "8"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648734698" then
+                                    fourth = "2"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648730082" then
+                                    fourth = "6"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648723237" then
+                                    fourth = "3"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648718450" then
+                                    fourth = "6"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648715920" then
+                                    fourth = "6"
+                                elseif v.Image == "http://www.roblox.com/asset/?id=9648712563" then
+                                    fourth = "2"
+                                end
+                                    end
+                                end
+                fireclickdetector(game.Workspace.Map.OriginOffice.Door.Keypad.Buttons.Reset.ClickDetector)
+                wait(0.25)
+                fireclickdetector(game.Workspace.Map.OriginOffice.Door.Keypad.Buttons[first].ClickDetector)
+                wait(0.25)
+                fireclickdetector(game.Workspace.Map.OriginOffice.Door.Keypad.Buttons[second].ClickDetector)
+                wait(0.25)
+                fireclickdetector(game.Workspace.Map.OriginOffice.Door.Keypad.Buttons[third].ClickDetector)
+                wait(0.25)
+                fireclickdetector(game.Workspace.Map.OriginOffice.Door.Keypad.Buttons[fourth].ClickDetector)
+                wait(0.25)
+                fireclickdetector(game.Workspace.Map.OriginOffice.Door.Keypad.Buttons.Enter.ClickDetector)
+                game:GetService("TeleportService"):Teleport(6403373529)
+                    ]])
+                end
+                game:GetService("TeleportService"):Teleport(9431156611)
+                end
+                      end    
+                })
+
+                BadgeTab:AddButton({
+                    Name = "Get Tycoon(10 min) arena only",
+                    Callback = function()
+                              repeat task.wait()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Arena.Plate.CFrame * CFrame.new(0,-1.5,0) * CFrame.Angles(math.rad(90), math.rad(0), math.rad(0))
+                until game.Players.LocalPlayer.PlayerGui.PlateIndicator.TextLabel.Text == "Plate Counter: 600"
+                                    end    
+                                })
 
 local PlayerTab = Window:MakeTab({
 	Name = "Player",
