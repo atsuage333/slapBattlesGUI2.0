4a24f23f439d648b49275150545637393928d00c
atsuage333 <127850785+atsuage333@users.noreply.github.com>
2024-02-04 20:54:28 +0900

    Update EternalBob.lua

--- EternalBob.lua
+++ EternalBob.lua
@@ -1,1 +1,2 @@
-
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/Mobile%20Friendly%20Orion')))()
+local Window = OrionLib:MakeWindow({Name = "[⚔Update]SlapBattles👏 EternalBob", HidePremium = false, IntroEnabled = false, SaveConfig = true, ConfigFolder = "OrionTest"})
