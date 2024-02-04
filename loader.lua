de96c38bc211115fe7a954b08bccb1fba31e14fa
atsuage333 <127850785+atsuage333@users.noreply.github.com>
2024-02-04 20:53:51 +0900

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 6403373529 then
   loadstring(game:HttpGet(("https://raw.githubusercontent.com/atsuage333/slapBattlesGUI2.0/main/main.lua")))()
 elseif  game.PlaceId == 16034567693 then
   loadstring(game:HttpGet(("https://raw.githubusercontent.com/atsuage333/slapBattlesGUI2.0/main/admin.lua")))()
+  elseif  game.PlaceId == 13833961666 then
+   loadstring(game:HttpGet(("https://raw.githubusercontent.com/atsuage333/slapBattlesGUI2.0/main/eternalbob.lua")))()
 end
