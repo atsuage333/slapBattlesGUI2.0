0634767bb51a6fe9c80829e7a3b713733e604891
atsuage333 <127850785+atsuage333@users.noreply.github.com>
2024-02-04 15:58:55 +0900

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,5 @@
-
+if game.PlaceId == 6403373529 then
+  loadstring(game:HttpGet(("https://raw.githubusercontent.com/atsuage333/slapBattlesGUI2.0/main/main.lua")))()
+elseif  game.PlaceId == 16034567693 then
+  loadstring(game:HttpGet(("https://raw.githubusercontent.com/atsuage333/slapBattlesGUI2.0/main/admin.lua")))()
+end
