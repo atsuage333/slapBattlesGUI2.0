83e62cd202da00e42627e1703c15769b8e140d72
atsuage333 <127850785+atsuage333@users.noreply.github.com>
2024-02-04 14:46:05 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ local HomeTab = Window:MakeTab({
 HomeTab:AddButton({
 	Name = "Go to Retro obby",
 	Callback = function()
-      		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-16976, 801.0068359375, 4907)
+      		print("dadsdadwa")
   	end    
 })	
 
