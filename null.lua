232db665dbbcab209ea169fc685fae40c99a085c
atsuage333 <127850785+atsuage333@users.noreply.github.com>
2024-02-05 16:18:45 +0900

    Update null.luasaddaas

--- null.lua
+++ null.lua
@@ -1,1 +1,3 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/Mobile%20Friendly%20Orion')))()
+local Window = OrionLib:MakeWindow({Name = "[⚔Update]SlapBattles👏", HidePremium = false, IntroEnabled = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
