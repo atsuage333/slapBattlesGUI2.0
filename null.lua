334332b0577b1a4260813c45357158748a3ca72b
atsuage333 <127850785+atsuage333@users.noreply.github.com>
2024-02-05 16:41:15 +0900

    null

--- null.lua
+++ null.lua
@@ -1,3 +1,31 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/Mobile%20Friendly%20Orion')))()
 local Window = OrionLib:MakeWindow({Name = "[⚔Update]SlapBattles👏", HidePremium = false, IntroEnabled = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
+local HomeTab = Window:MakeTab({
+	Name = "Home",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+HomeTab:AddButton({
+	Name = "Tp to NullGlove",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(5455.75244140625, -189.00051879882812, 1856.4912109375)
+  	end    
+})
+
+HomeTab:AddButton({
+	Name = "Tp to tinkerer",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(4834.14990234375, -214.0004425048828, 800.9007568359375)
+  	end    
+})
+
+HomeTab:AddButton({
+	Name = "Destroy Gui",
+	Callback = function()
+	      OrionLib:Destroy()
+  	end    
+})
+
+OrionLib:Init()
